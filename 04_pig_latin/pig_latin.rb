8eb18244cd5d109c7deb75df190764a71874221a
JrodManU <jrodmanu@yahoo.com>
2017-04-30 15:32:56 -0400

    finished 04_pig_latin (sans bonus)

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
 #write your code here
+def translate(phrase)
+  phrase.split(" ").map do |word|
+    until(word.match(/^[aeiou]/)) do
+      if(word.match(/.qu|qu/))
+        word += word[/.qu|qu/]
+        word.slice!(/.qu|qu/)
+      end
+      word += word[/^[^aeiou]*/]
+      word.slice!(/^[^aeiou]*/)
+    end
+    word += "ay"
+  end.join(" ")
+end
