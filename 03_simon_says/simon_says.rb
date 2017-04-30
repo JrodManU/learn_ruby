d92bf1bca378da83a3fde13295fcaa52a62cc06a
JrodManU <jrodmanu@yahoo.com>
2017-04-30 08:35:14 -0400

    Finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(phrase)
+  phrase
+end
+
+def shout(phrase)
+  phrase.upcase
+end
+
+def repeat(phrase, times = 2)
+  ((phrase + " ") * times)[0..-2]
+end
+
+def start_of_word(word, length)
+  word[0...length]
+end
+
+def first_word(phrase)
+  phrase.split(" ")[0]
+end
+
+def titleize(phrase)
+  phrase.split(" ").map.with_index do |word, i|
+    if(i == 0 || i == phrase.split(" ").size - 1 || word.length > 4)
+      word.capitalize
+    else
+      word
+    end
+  end.join(" ")
+end
