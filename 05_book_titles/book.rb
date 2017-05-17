28266b26d241bb694acd9b8c4968b917ed05fce8
JrodManU <jrodmanu@yahoo.com>
2017-05-16 22:03:37 -0400

    finished 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
 # write your code here
+  attr_accessor :title
+  def title
+    @title.split().map.with_index do |word, i|
+      if(!["and", "but", "a", "an", "the", "of", "in"].include?(word) || i == 0 || i == @title.split.length)
+        word.capitalize
+      else
+        word
+      end
+    end.join(" ")
+  end
 end
