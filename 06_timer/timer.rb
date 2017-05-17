c7aa824e63bf1b1cdc4abcd5b828d121b1ec5967
JrodManU <jrodmanu@yahoo.com>
2017-05-16 22:30:51 -0400

    finished 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+    super
+  end
+
+  def time_string
+    Time.at(@seconds).getlocal("+00:00").strftime("%T")
+  end
 end
